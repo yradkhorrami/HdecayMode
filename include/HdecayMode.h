3c574c490aa59d1fa1b9c9b564e022110bd5feea
Yasser Radkhorrami <yasser.radkhorrami@desy.de>
2020-05-12 18:58:51 +0200

    Create HdecayMode.h

---
+++ include/HdecayMode.h
@@ -0,0 +1,19 @@
+#ifndef HdecayMode_h
+#define HdecayMode_h 1
+
+#include "marlin/Processor.h"
+#include "lcio.h"
+#include <string>
+#include <TFile.h>
+#include <TTree.h>
+#include <vector>
+class TFile;
+class TH1F;
+class TH1I;
+class TH2I;
+class TTree;
+
+using namespace lcio ;
+using namespace marlin ;
+
+#endif
